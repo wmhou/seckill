c5a333a754ba5f9c3e63216292b2adb70e1853fe
wmhou <wm_hou@163.com>
2017-12-03 21:05:34 +0800

    秒杀业务系统dao层编码测试

---
+++ test/com/huawei/test/SeckillDaoTest.java
@@ -0,0 +1,41 @@
+package com.huawei.test;
+
+import com.huawei.dao.SeckillDao;
+import com.huawei.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+
+    //注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
+
+
+    @Test
+    public void queryById() throws Exception {
+        long seckillId=1000;
+        Seckill seckill=seckillDao.queryById(seckillId);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void queryAll() throws Exception {
+
+        List<Seckill> seckills=seckillDao.queryAll(0,100);
+        for (Seckill seckill : seckills)
+        {
+            System.out.println(seckill);
+        }
+    }
+}
---
+++ test/com/huawei/test/SuccessKilledDaoTest.java
@@ -0,0 +1,40 @@
+package com.huawei.test;
+
+import com.huawei.dao.SuccessKilledDao;
+import com.huawei.entity.SuccessKilled;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void insertSuccessKilled() throws Exception {
+        long seckillId=1000;
+        long userPhone=18700968920L;
+        int insertCount=successKilledDao.insertSuccessKilled(seckillId,userPhone);
+        System.out.println("insertCount="+insertCount);
+    }
+
+    @Test
+    public void queryByIdWithSeckill() throws Exception {
+
+        long seckillId=1000L;
+        long userPhone=18700968920L;
+        SuccessKilled successKilled=successKilledDao.queryByIdWithSeckill(seckillId,userPhone);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
+    }
+
+}
